429797609ed1923f514a42f4ab8cf3efd768e393
Davey <d.cornelissen8@gmail.com>
2020-03-11 20:12:18 +0100

    some changes

--- Fishare-microservices/auth-service/src/app.module.ts
+++ Fishare-microservices/auth-service/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 
 @Module({
   imports: [
     AuthModule,
-    UserModule,
     TypeOrmModule.forRoot({autoLoadEntities: true})
   ]
 })
