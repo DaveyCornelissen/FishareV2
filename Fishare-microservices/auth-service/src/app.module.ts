b23c5c7e589b5421495c7237773371d127bf5ef7
Davey <d.cornelissen8@gmail.com>
2020-03-12 17:03:36 +0100

    finnally got the docker compose connection working

--- Fishare-microservices/auth-service/src/app.module.ts
+++ Fishare-microservices/auth-service/src/app.module.ts
@@ -5,7 +5,10 @@ import { MongooseModule} from '@nestjs/mongoose'
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot('mongodb://admin:admin@localhost:27017/authentication'),
+    MongooseModule.forRoot('mongodb://fishare-auth-db:27017/authentication?authSource=admin', {
+      user: 'admin',
+      pass: 'admin'
+    }),
   ]
 })
 export class AppModule {}
